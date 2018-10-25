d89f7f83143a482acf2c6e2d2a4b1bbf785bce4c
katielynn.k <kkimble006@gmail.com>
2018-10-24 21:35:45 -0600

    Working full page dialog

--- src/App.js
+++ src/App.js
@@ -2,9 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import { NavigationDrawer } from 'react-md';
+import {
+  Button,
+  DialogContainer,
+  Divider,
+  TextField,
+  Toolbar,
+} from 'react-md';
+// import { PouchDB } from 'react-pouchdb/browser';
+// import Input from './Input';
 
 class App extends Component {
+
+  state = { visible: false, pageX: null, pageY: null };
+
+  show = (e) => {
+    // provide a pageX/pageY to the dialog when making visible to make the
+    // dialog "appear" from that x/y coordinate
+    let { pageX, pageY } = e;
+    if (e.changedTouches) {
+      pageX = e.changedTouches[0].pageX;
+      pageY = e.changedTouches[0].pageY;
+    }
+
+    this.setState({ visible: true, pageX, pageY });
+  };
+
+  hide = () => {
+    this.setState({ visible: false });
+  };
+
+  updateInputValue(evt) {
+    //debugger;
+    this.setState({
+      inputValue: evt
+    });
+
+    console.log(this.state.inputValue)
+  }
+
   render() {
+    const { visible, pageX, pageY } = this.state;
+
     return (
     <NavigationDrawer
       toolbarTitle="react-md with create-react-app v2"
@@ -25,6 +64,38 @@ class App extends Component {
               Learn React
             </a>
           </header>
+
+        <div>
+          <Button raised onClick={this.show} aria-controls="simple-full-page-dialog">
+            Open the Dialog
+          </Button>
+          <DialogContainer
+            id="simple-full-page-dialog"
+            visible={visible}
+            pageX={pageX}
+            pageY={pageY}
+            fullPage
+            onHide={this.hide}
+            aria-labelledby="simple-full-page-dialog-title"
+          >
+            <Toolbar
+              fixed
+              colored
+              title="New Event"
+              titleId="simple-full-page-dialog-title"
+              nav={<Button icon onClick={this.hide}>close</Button>}
+              actions={<Button flat onClick={this.hide}>Save</Button>}
+            />
+            <section className="md-toolbar-relative">
+              <TextField id="event-email" placeholder="Email" block paddedBlock />
+              <Divider />
+
+              <TextField id="event-name" placeholder="Event name" block paddedBlock />
+              <Divider />
+              <TextField id="event-desc" placeholder="Description" block paddedBlock rows={4} onChange={evt => this.updateInputValue(evt)} />
+            </section>
+          </DialogContainer>
+        </div>
       </div>
     </NavigationDrawer>
     );
